b8b9f7cc9728339f305cd8fdeef3761e37cf683f
cswasey16 <charlotte.swasey@gmail.com>
2016-04-11 18:07:26 -0400

    change in vote creation

---
+++ change_in_vote.R
@@ -0,0 +1,7 @@
+#percent change in vote choice, pre-post election
+
+#of postvoters, how many changed party?
+
+#pre votechoice is V083169a  1. Barack Obama 2. John McCain
+#postvote choice is V085044a 1. Barack Obama 2. John McCain
+
